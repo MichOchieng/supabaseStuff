6e6923138ad927cb6ee121f29acf3aadd4baff24
MichOchieng <MichOchieng@gmail.com>
2023-04-01 15:01:29 -0700

    Weird behaviour with context

--- context/user.js
+++ context/user.js
@@ -1,19 +1,53 @@
 import { supabase } from "@/utils/supabase";
 import { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from 'next/router'
 
 const Context = createContext();
 
 const Provider = ({ children }) => {
+    const router = useRouter()
     const [user,setUser] = useState(supabase.auth.getUser());
 
     useEffect(() => {
+        const getUserProfile = async () => {
+            const sessionUser = supabase.auth.getUser()
+            if (sessionUser) {
+                const { data: profile } = await supabase
+                .from('profile')
+                .select('*')
+                .eq('id',sessionUser.id)
+                .single()
+
+                setUser({
+                    ...sessionUser,
+                    ...profile
+                })
+            }
+        }
+
+        getUserProfile()
+
         supabase.auth.onAuthStateChange(() => {
-            setUser(supabase.auth.getUser())
+            getUserProfile()
         })
     },[])
 
+    const login = async () => {
+        await supabase.auth.signInWithOAuth({
+            provider: 'github',
+        })
+    }
+
+    const logout = async () => {
+        await supabase.auth.signOut()
+        setUser(null)
+        router.push('/')
+    }
+
     const exposed = {
         user,
+        login,
+        logout
     };
 
     return <Context.Provider value={exposed}>{children}</Context.Provider>
