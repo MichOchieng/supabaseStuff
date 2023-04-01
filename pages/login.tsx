e2491ca7006db909aa2a710e3c1cdaf1cfc5696a
MichOchieng <MichOchieng@gmail.com>
2023-04-01 01:23:55 -0700

    Added github oauth

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,13 @@
+import { supabase } from '@/utils/supabase'
 import React from 'react'
 
 const Login = () => {
+  React.useEffect(() => {
+    supabase.auth.signInWithOAuth({
+        provider: 'github',
+    })
+  })
+
   return (
     <div>Login</div>
   )
